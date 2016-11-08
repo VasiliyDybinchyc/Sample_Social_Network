08da3a65fbe3247040591f1f7429bfe8101b5cd7
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-08 15:16:03 +0000

    add user model, controller, migrate

---
+++ db/migrate/20161108151217_add_user.rb
@@ -0,0 +1,12 @@
+class AddUser < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string    :name
+      t.string    :email
+      t.string    :password_digest
+      t.string    :remember_digest
+
+      t.timestamps null: false
+    end
+  end
+end

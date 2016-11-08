4287e6a5c519ef5ba9683971780482523d1e06fa
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-08 15:59:04 +0000

    require capybara in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "capybara/rails"
 require 'database_cleaner'
 
 class ActiveSupport::TestCase

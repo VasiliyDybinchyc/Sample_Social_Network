965361e9ede3f2f54cc7d4e19a11e19378bcae8f
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-08 15:41:50 +0000

    add class IntegrationWebkitTest in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,26 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'database_cleaner'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  DatabaseCleaner.strategy = :truncation
+
+  def setup
+    DatabaseCleaner.start
+  end
+
+  def teardown
+    DatabaseCleaner.clean
+  end
+end
+
+class IntegrationWebkitTest < ActiveSupport::TestCase
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+  Capybara.default_wait_time = 5
+  Capybara.current_driver = :webkit
 end

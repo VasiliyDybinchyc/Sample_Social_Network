ffc39c20928c0ff4d5af8eec4572e37de1dd97c8
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-08 20:31:27 +0000

    small update views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,6 @@ class UsersController < ApplicationController
 
    def logged_in_user
       unless logged_in?
-        flash[:danger] = "Please log in."
         redirect_to root_path
       end
     end

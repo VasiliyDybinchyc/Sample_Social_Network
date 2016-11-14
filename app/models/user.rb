438e90cdb3cde2c54bb66cc2a51a16384b5ee31b
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-14 03:11:23 +0000

    small fix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -81,7 +81,7 @@ class User < ActiveRecord::Base
     following_ids = "SELECT followed_id FROM relationships
                      WHERE  follower_id = :user_id"
     Message.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+                     OR user_id = :user_id", user_id: id).order('created_at DESC')
   end
 
 end

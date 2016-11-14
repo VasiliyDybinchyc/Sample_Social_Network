2ced7c0e481d47a5415401a1b18a5614e9c00fa9
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-14 00:13:10 +0000

    add feed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @message  = current_user.messages.build
+    @feed_items = @user.feed
   end
 
   def all_frends

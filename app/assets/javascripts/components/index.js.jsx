d2f97ed07b8d02fbbf7040fdcfc125b4109dee97
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-21 02:30:29 +0000

    new index

---
+++ app/assets/javascripts/components/index.js.jsx
@@ -0,0 +1,10 @@
+var index = React.createClass({
+
+  render: function() {
+    return (
+      <div className="index">
+        <h1>Please <a href="/signup">Sign up</a> or <a href="/login">Log in</a> </h1>
+      </div>
+    );
+  }
+});

44c4c80fa5c399f0cca4fbc3ff8d1d393727010e
Vasiliy Dubinchyk <vasiliydybinchyk@gmail.com>
2016-11-21 02:29:45 +0000

    change feed

--- app/assets/javascripts/components/feed.js.jsx
+++ app/assets/javascripts/components/feed.js.jsx
@@ -1,36 +1,58 @@
-var Article = React.createClass({
-  propTypes: {
-    feed_item: React.PropTypes.shape({
-      content: React.PropTypes.string.isRequired,
-    })
+var News = React.createClass({
+
+  getInitialState: function() {
+    return {
+      message: "not at bottom",
+      end: 10,
+    };
+  },
+
+  handleScroll() {
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      this.setState({
+        message:'bottom reached',
+        end: end + 10
+      })
+
+    } else {
+      this.setState({
+        message:'not at bottom'
+      });
+    }
+  },
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
 
   render: function() {
-    var content = this.props.feed_item.content;
 
-    return (
-      <div>
-        <p>message id {this.props.feed_item.id} :</p>
-        <p>{content}</p>
-      </div>
-    )
-  }
-});
-
-var News = React.createClass({
-  render: function() {
   FeedItemsLength = this.props.feed_items.length;
+
+  end = this.state.end
+
   if (FeedItemsLength > 0) {
-    FeedItems = this.props.feed_items.map( function(feed_item) {
+    FeedItems = this.props.feed_items.slice(0, end).map( function(feed_item, index) {
         return (
-          <div key={feed_item.id}>
-            <Article feed_item={feed_item} />
+          <div key={index} onScroll={this.handleScroll}>
+            <p>message id {feed_item.id} :</p>
+            <p>{feed_item.content}</p>
           </div>
         );
       });
     } else {
       FeedItems = <p>К сожалению новостей нет</p>
     }
+
     return (
       <div className="Feed">
         <h1>News</h1>

a0f7be9e36a1a7c762a090ff7784687aaf9e7c6e
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-02-28 12:29:12 +0200

    reactstrap page login

--- frontend/components/views/create_session.js
+++ frontend/components/views/create_session.js
@@ -1,28 +1,43 @@
 import React from 'react';
+import {  Button,
+          Form,
+          FormGroup,
+          Label,
+          Input }         from 'reactstrap';
+
+import { fourSpaces }     from '../../helper/helperFrontend';
 
 export default React.createClass({
 
   getEmail: function() {
-    return this.refs.email.value;
+    return this.email.value;
   },
 
   getPassword: function() {
-    return this.refs.password.value;
+    return this.password.value;
   },
 
   render: function() {
     return (
       <div>
-        <div>Log In</div>
-        <div className="user-profile">
-          <form onSubmit={this.props.onSubmit} >
-            <input type="text" ref="email" placeholder="Email" />
-            <input type="text" ref="password" placeholder="Password" />
-            <button>Log In</button>
-          </form>
-        </div>
+        <h1>Log In</h1>
+
+        <Form inline>
+          <FormGroup>
+            <Label for="LoginEmail">Email</Label>{fourSpaces}
+            <Input type="email" name="LoginEmail" getRef={(ref) => (this.email = ref)} id="LoginEmail" placeholder="Email" />
+          </FormGroup>{fourSpaces}
+
+          <FormGroup>
+            <Label for="LoginPassword">Password</Label>{fourSpaces}
+            <Input type="password" name="LoginPassword" getRef={(ref) => (this.password = ref)} id="LoginPassword" placeholder="Password" />
+          </FormGroup>{fourSpaces}
+
+          <Button color="success" onClick={this.props.onSubmit}>
+            Log in
+          </Button>
+        </Form>
       </div>
     );
   }
-
 });

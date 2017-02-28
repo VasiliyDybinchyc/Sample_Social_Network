8ec6dd2c77c55c8d5030653b540e7a0f67edebc3
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-02-28 10:25:09 +0200

    litle beter not login nav

--- frontend/components/views/not_login_nav.js
+++ frontend/components/views/not_login_nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { browserHistory } from 'react-router';
-import { Navbar, Nav, NavbarBrand, NavItem, Collapse } from 'reactstrap';
+import { Navbar, Nav, NavbarBrand, NavItem, Collapse, Button } from 'reactstrap';
 
 export default class NotLoginNav extends React.Component{
 
@@ -20,15 +20,15 @@ export default class NotLoginNav extends React.Component{
 
         <Nav className="ml-auto" navbar>
           <NavItem className="col-8">
-            <button type="button" className="btn btn-primary" onClick={this.handleClickSignUp}>
+            <Button color="primary" onClick={this.handleClickSignUp}>
               Sign up
-            </button>
+            </Button>
           </NavItem>
 
           <NavItem className="col-9">
-            <button type="button" className="btn btn-success" onClick={this.handleClickLogin}>
+            <Button color="success" onClick={this.handleClickLogin}>
               Log in
-            </button>
+            </Button>
           </NavItem>
         </Nav>
       </Navbar>

46e58f612bedbee469fa97acf10cd3a96af9d183
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-03-08 11:16:46 +0200

    again add max width for avatar

--- frontend/components/views/personal_info.js
+++ frontend/components/views/personal_info.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import ReactCrop from 'react-image-crop'
+
 export default class PersonalInfo extends React.Component{
 
   render() {
     return (
     <div className='PersonalInfo'>
       <h2>{this.props.user.first_name} {this.props.user.last_name}</h2>
-      <img src={this.props.user.avatar.url} width="300" height="500" ></img>
-      {/* I don know why but if i just point out in src this.props.user.avatar.url on page another user will be error */}
+      <img src={this.props.user.avatar.url} style={{ maxWidth: 200 }} ></img>
     </div>
     );
   }
 };
+{/* <link rel='stylesheet' href='ReactCrop.css'/>
+<ReactCrop src={this.props.user.avatar.url} crop={crop} /> */}

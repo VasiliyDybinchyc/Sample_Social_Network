c5bef9051bdd170c5aeb521fe758f3f9b066f2d7
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-03-09 10:41:09 +0200

    webpack dont see change in file, its just test

--- frontend/components/views/personal_info.js
+++ frontend/components/views/personal_info.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-import ReactCrop from 'react-image-crop'
-
 export default class PersonalInfo extends React.Component{
 
   render() {
     return (
     <div className='PersonalInfo'>
-      <h2>{this.props.user.first_name} {this.props.user.last_name}</h2>
-      <img src={this.props.user.avatar.url} style={{ maxWidth: 200 }} ></img>
+      
+      <img src={this.props.user.avatar.url} style={{ maxWidth: 300 }} ></img>
     </div>
     );
   }
 };
-{/* <link rel='stylesheet' href='ReactCrop.css'/>
-<ReactCrop src={this.props.user.avatar.url} crop={crop} /> */}

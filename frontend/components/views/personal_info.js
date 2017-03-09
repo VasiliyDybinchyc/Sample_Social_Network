b27d283750f4249feea530ece77aa0b5c3b87db0
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-03-09 12:08:39 +0200

    Fix webpack p.s. he yourself

--- frontend/components/views/personal_info.js
+++ frontend/components/views/personal_info.js
@@ -5,7 +5,7 @@ export default class PersonalInfo extends React.Component{
   render() {
     return (
     <div className='PersonalInfo'>
-      
+      <h2>{this.props.user.first_name} {this.props.user.last_name}</h2>
       <img src={this.props.user.avatar.url} style={{ maxWidth: 300 }} ></img>
     </div>
     );

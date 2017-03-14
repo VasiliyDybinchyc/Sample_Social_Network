c5ed22b3d92d145506e8cc5cca067c1134eb71d8
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-03-14 14:41:29 +0200

    add lightbox for news image 'alfa'

--- frontend/components/views/user_news.js
+++ frontend/components/views/user_news.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { checkIfYourOnBottomPage, changeStatusModal } from '../../helper/helperFrontend';
 
+import Lightbox from 'react-image-lightbox';
+
+var images = [
+
+];
+
 export default class Messages extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { end: 10};
+    this.state = { start:0,
+                   end: 10,
+                   photoIndex: 0,
+                   isOpen: false};
   }
 
   handleScroll() {
+    var stateStart = this.state.start;
     var stateEnd = this.state.end;
     if (checkIfYourOnBottomPage()) {
       this.setState({
-        end: stateEnd + 10
+        end: stateEnd + 10,
+        start: stateStart + 10
       })
     }
   }
@@ -26,21 +37,19 @@ export default class Messages extends React.Component {
     window.removeEventListener("scroll", this.Scroll);
   }
 
-  getImageUrl() {
-    return this.urlImage;
-  }
-
-  testFunction(url){
-    this.urlImage = url;
-    changeStatusModal(!this.props.status)
-  }
-
   render() {
+
+    const {
+               photoIndex,
+               isOpen,
+           } = this.state;
 
   var FeedItems,
       FeedItemsLength = this.props.feed_items.length,
+      start = this.state.start,
       end = this.state.end,
       that = this;
+
   if (FeedItemsLength > 0) {
     FeedItems = this.props.feed_items.slice(0, end).map( function(feed_item, index) {
         return (
@@ -49,20 +58,39 @@ export default class Messages extends React.Component {
             <p dangerouslySetInnerHTML={{__html: feed_item.content}} />
             {feed_item.picture.url == null ? null : < img src={feed_item.picture.url}
                                                       alt="lorem"
-                                                      onClick={() => that.testFunction(feed_item.picture.url) } />}
+                                                      onClick={() => {images.push(feed_item.picture.url, null), that.setState({ isOpen: true }) } } /> }
           </div>
         );
       });
     } else {
       FeedItems = <p>"You don't have news"</p>
     }
-
     return (
       <div className="Feed">
         <h1>News</h1>
         <div id="News">
           <strong className={FeedItemsLength > 0 ? 'All-news-title':'none'}>All news: {FeedItemsLength}</strong>
           {FeedItems}
+          <button type="button"
+                  onClick={() => this.setState({ isOpen: true })}>
+                    Open Lightbox
+                </button>
+
+                {isOpen &&
+                    <Lightbox
+                        mainSrc={images[photoIndex]}
+                        nextSrc={images[(photoIndex + 1) % images.length]}
+                        prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+
+                        onCloseRequest={() => {this.setState({ isOpen: false }), images=[] }}
+                        onMovePrevRequest={() => this.setState({
+                            photoIndex: (photoIndex + images.length - 1) % images.length,
+                        })}
+                        onMoveNextRequest={() => this.setState({
+                            photoIndex: (photoIndex + 1) % images.length,
+                        })}
+                    />
+                }
         </div>
       </div>
     );

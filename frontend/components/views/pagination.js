811f5d5d112dbc30db997b4b10a4a2fe2042f456
Vasiliy Dubinchuk <sg.vasilyD@gmail.com>
2017-03-15 01:25:47 +0200

    beter pagination

--- frontend/components/views/pagination.js
+++ frontend/components/views/pagination.js
@@ -10,34 +10,37 @@ export default class PaginationView extends React.Component {
     }
   }
 
-  render() {
-
-    let defaultPosition = [1, 2, 3, 4, 5]
-    if (this.props.pageNumber <= 3) {
-      this.position = defaultPosition
-    }else if (Math.round(this.amountPagination) - Math.round(this.props.pageNumber) <= 1){
-      this.position = this.position
-    }else {
-      for (let i = 0; i < this.position.length; i++) {
-        this.position[i] = Number(this.props.pageNumber) - 2 + i
-      }
-    }
-
-
-    var nextPage = Number(this.props.pageNumber) + 1,
-        previousPage = Number(this.props.pageNumber) - 1;
-
+  componentWillMount(){
     var amountElement = 0
-
     if (this.props.paginationFor == "galerey") {
       this.props.amountElement.forEach( function(galerey, index) {
           return (
               galerey.picture.url == null ? null : amountElement += 1
           );
         });
+        this.amountPagination = Math.ceil(amountElement / 7)
+    }
+  }
+
+  render() {
+    this.position = [,,,,,]
+    let defaultPosition = [1, 2, 3, 4, 5];
+    if (this.props.pageNumber <= 3) {
+      this.position = defaultPosition
+    }else if (Math.round(this.amountPagination) - Math.round(this.props.pageNumber) <= 2){
+      if (this.position[4] !== this.amountPagination){
+        for (let i = 0; i < this.position.length; i++) {
+          this.position[i] = this.amountPagination - 4 + i
+        }
+      }
+    }else {
+      for (let i = 0; i < this.position.length; i++) {
+        this.position[i] = Number(this.props.pageNumber) - 2 + i
+      }
     }
 
-    this.amountPagination = Math.round(amountElement) / 7
+    var nextPage = Number(this.props.pageNumber) + 1,
+        previousPage = Number(this.props.pageNumber) - 1;
 
     var viewPagination = []
 
